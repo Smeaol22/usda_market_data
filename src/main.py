86b93f37ead4e67a857b0ef312f8c9398ac14699
Olivier D <olivier.dauloudet@hotmail.com>
2021-04-30 15:58:10 +0200

    fix function definition

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import requests
 
 from src.conf import UsdaMarketUrl
@@ -7,11 +6,15 @@ from src.utils import convert_report_to_df
 
 
 def retrieve_all_published_reports():
+    """
+        This function is useful to load all possible USDA market data into a dataframe
+    Returns:
+        (dataframe): with all possible data from UsdaMarketUrl.ALL_REPORTS_SUMMARY
+    """
     response = requests.get(UsdaMarketUrl.ALL_REPORTS_SUMMARY)
     if response.status_code >= 200 or response.status_code < 300:
         published_reports_info_df = convert_report_to_df(response.content, column_line=3, missed_lines=[0, 1, 2, 3])
-        published_reports_info_df_complete =
     else:
         raise UsdaMarketRequestError(
             f"Request failed to retrieve all published reports with code {response.status_code}")
-    return published_reports_info_df_complete
+    return published_reports_info_df
