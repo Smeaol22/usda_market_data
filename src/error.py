a271b411466c95d44cd23df50089bb8786799f04
Olivier D <olivier.dauloudet@hotmail.com>
2021-05-07 14:27:21 +0200

    add error code

--- src/error.py
+++ src/error.py
@@ -8,3 +8,7 @@ class UsdaMarketInternalError(Exception):
     def __init__(self, message, errors):
         super().__init__(message)
         self.errors = errors
+
+
+class ErrorCode(enumerate):
+    CONVERSION_ERROR = 'CONVERSION_ERROR'
